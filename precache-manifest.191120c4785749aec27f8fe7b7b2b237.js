8881b8bd030d0651778a34f820554acaf1682522
Francisco <franciscoparisalbero@gmail.com>
2021-04-15 15:28:08 +0200

    Updates

---
+++ precache-manifest.191120c4785749aec27f8fe7b7b2b237.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "14c5f31b06b989fba12d1a87e31326a8",
+    "url": "/web_tnk/static/media/logo_copy.14c5f31b.png"
+  },
+  {
+    "revision": "4a90499a868abccac3e8",
+    "url": "/web_tnk/static/js/runtime~main.4127f75a.js"
+  },
+  {
+    "revision": "1354b79cf2facc02b7d6",
+    "url": "/web_tnk/static/js/main.4231d698.chunk.js"
+  },
+  {
+    "revision": "7d1b44e927343c355fd1",
+    "url": "/web_tnk/static/js/2.a7af2a1d.chunk.js"
+  },
+  {
+    "revision": "1354b79cf2facc02b7d6",
+    "url": "/web_tnk/static/css/main.7184ca8a.chunk.css"
+  },
+  {
+    "revision": "dc4c0cda5696d2c74e1166bb1e877a0a",
+    "url": "/web_tnk/index.html"
+  }
+];
