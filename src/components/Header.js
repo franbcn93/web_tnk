8ffdf41723d8694ac0c04c3c4e5aac9f2b08e5ab
Francisco <franciscoparisalbero@gmail.com>
2021-04-15 15:17:30 +0200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,64 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+import React, { useState, useEffect } from "react";
+import "../style/Header.css";
+import { CSSTransition } from "react-transition-group";
+
+export default function Header() {
+  const [isNavVisible, setNavVisibility] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 700px)");
+    mediaQuery.addListener(handleMediaQueryChange);
+    handleMediaQueryChange(mediaQuery);
+
+    return () => {
+      mediaQuery.removeListener(handleMediaQueryChange);
+    };
+  }, []);
+
+  const handleMediaQueryChange = (mediaQuery) => {
+    if (mediaQuery.matches) {
+      setIsSmallScreen(true);
+    } else {
+      setIsSmallScreen(false);
+    }
+  };
+
+  const toggleNav = () => {
+    setNavVisibility(!isNavVisible);
+  };
+
+  return (
+    <header className="Header">
+      <img
+        src={require("../assets/logo_copy.png")}
+        className="Logo"
+        alt="logo"
+      />
+      <CSSTransition
+        in={!isSmallScreen || isNavVisible}
+        timeout={350}
+        classNames="NavAnimation"
+        unmountOnExit
+      >
+        <nav className="Nav">
+          <a href={("/", "/react_web")}>Inicio</a>
+          <a href="/gestion">Tecnología e innovación</a>
+          <a href="/hospitales">Hospitales</a>
+          <a href={"/contacto"}>Contacto</a>
+          {/* <button>Logout</button> */}
+        </nav>
+      </CSSTransition>
+      <button onClick={toggleNav} className="Burger">
+        <img
+          src="https://res.cloudinary.com/dfb6ivozp/image/upload/v1618491735/menu_zfpcfn.png"
+          alt="Girl in a jacket"
+          width="500"
+          height="600"
+          className="menuOpen"
+        ></img>
+      </button>
+    </header>
+  );
+}
